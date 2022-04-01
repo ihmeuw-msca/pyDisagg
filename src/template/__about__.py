b7c10b168913f6ee8ddf710e76b82afee7957409
zhengp0 <zhengp@uw.edu>
2022-03-31 21:03:16 -0700

    Initial commit

---
+++ src/template/__about__.py
@@ -0,0 +1,21 @@
+import datetime
+
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+]
+
+__title__ = "template"
+__summary__ = ""
+__uri__ = "https://github.com/ihmeuw-msca/template"
+__version__ = "0.0.0"
+__author__ = "IHME Math Sciences"
+__email__ = "ihme.math.sciences@gmail.com"
+__license__ = "BSD 2-Clause License"
+__copyright__ = f"{datetime.date.today().year} {__author__}"
