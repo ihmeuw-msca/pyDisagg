25b46073a7c10615d19df739fb1daa76d1210640
AHsu98 <34590951+AHsu98@users.noreply.github.com>
2023-03-02 22:06:27 -0800

    cleaned up disaggregate a bit more
    Fixed bug with argument order, got rid of split_dataframe_rate

--- src/pydisagg/disaggregate.py
+++ src/pydisagg/disaggregate.py
@@ -14,8 +14,8 @@ def split_datapoint(
     bucket_populations: NDArray,
     rate_pattern: NDArray,
     observed_total_se: Optional[float] = None,
-    output_type: str = 'total',
     model: Optional[DisaggModel] = LogOdds_model(),
+    output_type: str = 'total',
     CI_method: Optional[str] = 'delta-wald'
 ) -> Union[tuple,NDArray]:
     """Disaggregate a datapoint using the model given as input.
@@ -74,7 +74,7 @@ def split_datapoint(
             rate_pattern,
             CI_method=CI_method
         )
-    elif output_type=='rate':
+    if output_type=='rate':
         return model.split_groups_rate(
             bucket_populations,
             observed_total,
@@ -92,9 +92,9 @@ def split_dataframe(
     observation_group_membership_df: DataFrame,
     population_sizes: DataFrame,
     rate_patterns: DataFrame,
-    output_type: str = 'total',
     use_se: Optional[bool] = False,
-    model: Optional[DisaggModel] = LMO_model(1),
+    model: Optional[DisaggModel] = LogOdds_model(),
+    output_type: str = 'total',
 ) -> DataFrame:
     """Disaggregate datapoints and pivots observations into estimates for each group per demographic id
 
@@ -185,95 +185,3 @@ def split_dataframe(
                            'estimate', 'se'], axis=1)  # .groupby(level=0).sum()
 
     return result
-
-def split_dataframe_rate(
-    groups_to_split_into: list,
-    observation_group_membership_df: DataFrame,
-    population_sizes: DataFrame,
-    rate_patterns: DataFrame,
-    use_se: Optional[bool] = False,
-    model: Optional[DisaggModel] = LMO_model(1),
-) -> DataFrame:
-    """Disaggregate datapoints and pivots observations into rate estimates for each group per demographic id
-
-    Parameters
-    ----------
-    groups_to_split_into : list
-        list of groups to disaggregate observations into
-    observation_group_membership_df : DataFrame
-        Dataframe with columns demographic_id, pattern_id, obs,
-        and columns for each of the groups_to_split_into
-        with dummy variables that represent whether or not
-        each group is included in the observations for that row.
-        This also optionally contains a obs_se column which will be used if use_se is True
-        demographic_id represents the population that the observation comes from
-        pattern_id gives the baseline that should be used for splitting
-    population_sizes : DataFrame
-        Dataframe with demographic_id as the index containing the
-        size of each group within each population (given the demographic_id)
-    rate_patterns : DataFrame
-        dataframe with pattern_id as the index, and columns
-        for each of the groups_to_split where the entries represent the rate pattern
-        in the given group to use for pydisagg.
-    use_se : Optional[bool], optional
-        whether or not to report standard errors along with estimates
-        if set to True, then observation_group_membership_df must have an obs_se column
-        , by default False
-    model : Optional[DisaggModel], optional
-        DisaggModel to use for splitting, by default LMO_model(1)
-
-    Returns
-    -------
-    DataFrame
-        Dataframe where each row corresponds to one of obs, with one or
-            two columns for each of the groups_to_split_into, giving the rate estimate
-        If use_se==True, then has a nested column indexing, where both the
-            point estimate and standard error for the estimate for each group is given.
-    """
-    splitting_df = observation_group_membership_df.copy()
-    if use_se is False:
-        def split_row(x):
-            return split_datapoint_rate(
-                x['obs'],
-                population_sizes.loc[x.name]*x[groups_to_split_into],
-                rate_patterns.loc[x['pattern_id']],
-                model=model
-            )
-        result = (
-            splitting_df
-            .set_index('demographic_id')
-            .apply(
-                split_row,
-                axis=1)
-            .reset_index()
-            # .groupby('demographic_id')
-            # .sum()
-        )
-    else:
-        def split_row(x):
-            raw_split_result = split_datapoint_rate(
-                x['obs'],
-                population_sizes.loc[x.name]*x[groups_to_split_into],
-                rate_patterns.loc[x['pattern_id']],
-                model=model,
-                observed_total_se=x['obs_se']
-            )
-            return pd.Series(
-                [
-                    (estimate, se) for estimate, se in zip(raw_split_result[0], raw_split_result[1])
-                ],
-                index=groups_to_split_into)
-        result_raw = (
-            splitting_df
-            .set_index('demographic_id')
-            .apply(
-                split_row,
-                axis=1)
-        )
-        point_estimates = result_raw.applymap(lambda x: x[0])
-        standard_errors = result_raw.applymap(lambda x: x[1])
-        result = pd.concat([point_estimates, standard_errors], keys=[
-                           'rate_estimate', 'se'], axis=1)  # .groupby(level=0).sum()
-
-    return result
-
